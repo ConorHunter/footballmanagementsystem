557907b5b77071ffd120da0c1cdec73c5df0b6f9
Conor <chunter668@hotmail.com>
2020-09-17 16:37:45 +0100

    Updates

---
+++ precache-manifest.10718f9d393379ee7f48e1cede040999.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0116d4fdb7b7acc36405db8f7ae069bd",
+    "url": "/footballmanagementsystem/index.html"
+  },
+  {
+    "revision": "45cbeb23a3cfcb50549e",
+    "url": "/footballmanagementsystem/static/css/2.f93178fb.chunk.css"
+  },
+  {
+    "revision": "9453cb0719677cc593cf",
+    "url": "/footballmanagementsystem/static/css/main.32ecc9b5.chunk.css"
+  },
+  {
+    "revision": "45cbeb23a3cfcb50549e",
+    "url": "/footballmanagementsystem/static/js/2.5ac7da7a.chunk.js"
+  },
+  {
+    "revision": "b21c0b070fbf389a149e773557d68a8a",
+    "url": "/footballmanagementsystem/static/js/2.5ac7da7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9453cb0719677cc593cf",
+    "url": "/footballmanagementsystem/static/js/main.743cfd4a.chunk.js"
+  },
+  {
+    "revision": "172a329f77b75b75ab3b",
+    "url": "/footballmanagementsystem/static/js/runtime-main.b982e0ca.js"
+  }
+]);
